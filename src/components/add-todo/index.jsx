159e052edc25fb228f273c569ac7db9647ef09b3
Amit Chawla <amit.chawla2@sapient.com>
2020-05-09 19:36:17 +0530

    Changed class to hooks

--- src/components/add-todo/index.jsx
+++ src/components/add-todo/index.jsx
@@ -1,51 +1,38 @@
-import React, { PureComponent } from "react";
+import React, { useState } from "react";
 
-class AddTodo extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ""
-    };
-  }
+const AddTodo = ({ addTodoHandler }) => {
+  const [text, onChange] = useState("");
 
-  onChangeHandler = event => {
-    this.setState({ text: event.target.value });
-  };
-
-  onAddClick = event => {
-    const { addTodoHandler } = this.props;
-    const { text } = this.state;
+  const onAddClick = event => {
     if (text) {
-      this.setState({ text: "" });
+      onChange("");
       addTodoHandler(text);
     }
   };
 
-  render() {
-    return (
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter Todo"
-          aria-label="Enter Todo"
-          aria-describedby="button-addon2"
-          onChange={this.onChangeHandler}
-          value={this.state.text}
-        />
-        <div className="input-group-append">
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="button-addon2"
-            onClick={this.onAddClick}
-          >
-            Add
-          </button>
-        </div>
+  return (
+    <div className="input-group mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter Todo"
+        aria-label="Enter Todo"
+        aria-describedby="button-addon2"
+        onChange={e => onChange(e.target.value)}
+        value={text}
+      />
+      <div className="input-group-append">
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+          onClick={onAddClick}
+        >
+          Add
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default AddTodo;
--- src/components/todos/index.jsx
+++ src/components/todos/index.jsx
@@ -1,63 +1,45 @@
-import React, { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidV4 } from "uuid";
 import AddTodo from "../add-todo";
 import TodoList from "../todo-list";
 
-class Todos extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: []
-    };
-  }
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
 
-  componentDidMount() {
-    this.setState({ todos: JSON.parse(localStorage.getItem("todos")) || [] });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { todos: preTodos } = prevState;
-    const { todos } = this.state;
-    if (todos !== preTodos) {
-      localStorage.setItem("todos", JSON.stringify(todos));
-    }
-  }
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("todos") || "[]"));
+  }, []);
 
-  addTodoHandler = text => {
-    const { todos } = this.state;
-    this.setState({
-      todos: [...todos, { id: uuidV4(), text, completed: false }]
-    });
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodoHandler = text => {
+    setTodos([...todos, { id: uuidV4(), text, completed: false }]);
   };
 
-  onCompletedHandler = ({ id, completed }) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.map(todo =>
+  const onCompletedHandler = ({ id, completed }) => {
+    setTodos(
+      todos.map(todo =>
         todo.id === id ? { ...todo, completed: !completed } : todo
       )
-    });
-  };
-
-  onDeleteHandler = ({ id }) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.filter(todo => todo.id !== id)
-    });
-  };
-
-  render() {
-    const { todos } = this.state;
-    return (
-      <>
-        <AddTodo addTodoHandler={this.addTodoHandler} />
-        <TodoList
-          todos={todos}
-          onCompletedHandler={this.onCompletedHandler}
-          onDeleteHandler={this.onDeleteHandler}
-        />
-      </>
     );
-  }
-}
+  };
+
+  const onDeleteHandler = ({ id }) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <>
+      <AddTodo addTodoHandler={addTodoHandler} />
+      <TodoList
+        todos={todos}
+        onCompletedHandler={onCompletedHandler}
+        onDeleteHandler={onDeleteHandler}
+      />
+    </>
+  );
+};
 
 export default Todos;
