97d913498ea4c99f95676ceee0878a3c51b6a25c
Amit Chawla <amit.chawla2@sapient.com>
2020-05-09 15:51:40 +0530

    Removed carousel code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from "react";
-import Carousel from "./components/Carousel";
 
-/*
-  persist autoplay here
- */
 function App() {
   return (
     <div className="container">
       <h1>React Hooks Training</h1>
-      <main>
-        <Carousel />
-        {/* <div className="d-flex justify-content-center m-2">
-          <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-secondary">
-              Forward
-            </button>
-            <button type="button" class="btn btn-secondary">
-              Stop
-            </button>
-            <button type="button" class="btn btn-secondary">
-              Reverse
-            </button>
-          </div>
-        </div> */}
-      </main>
+      <main></main>
     </div>
   );
 }
